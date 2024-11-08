d31477f831b364a9fc2856aefa18498b7c714449
Auckloo Veevek <avish@Auckloos-MacBook-Pro.local>
2024-11-07 21:54:17 -0500

    Created style.css to add basic styling to blog

---
+++ script.js
@@ -0,0 +1,8 @@
+const createButton = document.getElementById("create-post");
+const editButton = document.getElementById("edit-post");
+const reloadButton = document.getElementById("reload-btn");
+
+reloadButton.addEventListener("click", () => {
+    window.location.reload();
+
+})
